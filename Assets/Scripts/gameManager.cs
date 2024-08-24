41a0dec94467a1e12dd0ca25dbe25c7b838bfb5a
NikolasLukic <135436448+NikolasLukic@users.noreply.github.com>
2024-08-25 00:59:13 +1000

    Canvas and pause menu and pause functionality added

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    public GameObject pauseMenu; 
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            Pause(); 
+        }
+    }
+
     public void Pause()
     {
         if (Time.timeScale == 1)
         {
-            Time.timeScale = 0; 
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true); 
         }else
         {
-            Time.timeScale = 1; 
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false); 
         }
     }
 }
