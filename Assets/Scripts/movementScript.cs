d5a780b975c1422107480fc4bd09e1694d9963fb
NikolasLukic <135436448+NikolasLukic@users.noreply.github.com>
2024-08-24 17:04:26 +1000

    Player and its movement added

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class movementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float groundCheckRadius = 0.2f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get input from A/D or left/right arrow keys
+        float moveX = Input.GetAxis("Horizontal");
+
+        // Apply movement velocity
+        Vector2 velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        rb.velocity = velocity;
+
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Check for jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Visualize the ground check circle in the scene view
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
