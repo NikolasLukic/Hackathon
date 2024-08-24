be9a77501baf5668151a7b41f1270d84b60d150b
NikolasLukic <135436448+NikolasLukic@users.noreply.github.com>
2024-08-24 19:14:56 +1000

    Character animations in place

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -12,9 +12,14 @@ public class movementScript : MonoBehaviour
     private bool isGrounded;
     private float groundCheckRadius = 0.2f;
 
+    public SpriteRenderer sprite;
+
+    public Animator animator; 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); 
     }
 
     void Update()
@@ -22,12 +27,29 @@ public class movementScript : MonoBehaviour
         // Get input from A/D or left/right arrow keys
         float moveX = Input.GetAxis("Horizontal");
 
+        // Flip player based on movement direction
+        if (moveX > 0)
+        {
+            sprite.flipX = false;
+            animator.SetBool("moving", true);
+        }
+        else if (moveX < 0)
+        {
+            sprite.flipX = true;
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false); 
+        }
+
         // Apply movement velocity
         Vector2 velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
         rb.velocity = velocity;
 
         // Check if the player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        animator.SetBool("grounded", isGrounded); 
 
         // Check for jump input
         if (Input.GetButtonDown("Jump") && isGrounded)
